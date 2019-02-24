51e375e50335c626bb43e5ab1096d7b4c420507b
Yusuke Sangenya <longinus.eva@gmail.com>
2019-02-24 20:46:01 +0900

    introduce haskeline (tab completion & history)

--- src/Main.hs
+++ src/Main.hs
@@ -2,27 +2,35 @@
 module Main where
 
 import System.Environment (lookupEnv)
-import System.IO (stdout, stdin, stderr, hFlush)
+import System.IO (stdout, stdin, stderr, hFlush, hSetBuffering, hGetChar, BufferMode(..))
 import System.Exit (ExitCode(..), exitWith)
+import System.Console.Haskeline
 import Data.Maybe (fromMaybe)
 import Control.Exception (catch, IOException)
+import Control.Monad.IO.Class (liftIO)
 
 import Hash.Utils (forkWait)
 import Hash.Parser (parseLine)
 import Hash.Evaluator (execExpr)
 
-prompt = do
-  p <- fromMaybe "Hash> " <$> lookupEnv "PROMPT"
-  putStr p >> hFlush stdout
-
+prompt = fromMaybe "Hash> " <$> lookupEnv "PROMPT"
+inputSettings = Settings { complete = completeFilename, historyFile = Just "/home/sangenya/.hash_history", autoAddHistory = True }
+ 
 main :: IO ()
-main = do
-  prompt
-  line <- catch getLine $ \(_ :: IOException) -> exitWith ExitSuccess
-  case parseLine line of
-    Left err -> print err
-    Right expr -> do
-      execExpr (stdin, stdout) expr
-      hFlush stdout
-      return ()
-  main
+main = runInputT inputSettings repl
+  where
+    repl :: InputT IO ()
+    repl = do
+      minput <- liftIO prompt >>= getInputLine
+      case minput of
+        Nothing -> return ()
+        Just "" -> repl
+        Just line -> do
+          liftIO $ do
+            case parseLine line of
+              Left err -> print err
+              Right expr -> do
+                execExpr (stdin, stdout) expr
+                hFlush stdout
+                return ()
+          repl
